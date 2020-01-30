e6c44542f4bb513db1b7fa1659939b54c812bf2e
yuanguangxin <274841922@qq.com>
2020-01-31 02:41:52 +0800

    add q21

---
+++ src/q21/f2/ListNode.java
@@ -0,0 +1,10 @@
+package q21.f2;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ src/q21/f2/Solution.java
@@ -0,0 +1,20 @@
+package q21.f2;
+
+/**
+ * 递归（看成两个链表头部较小的一个与剩下元素的 merge 操作结果合并） o(n)
+ */
+public class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        } else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+}
