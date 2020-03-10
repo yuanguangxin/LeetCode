1fbb4281b39649c13d7f3f429dac0ec51409707b
yuanguangxin <274841922@qq.com>
2020-03-10 22:51:19 +0800

    add q226

---
+++ src/递归/q226_翻转二叉树/Solution.java
@@ -0,0 +1,23 @@
+package 递归.q226_翻转二叉树;
+
+/**
+ * 递归 o(n)
+ */
+public class Solution {
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        if (root.left != null) {
+            invertTree(root.left);
+        }
+        if (root.right != null) {
+            invertTree(root.right);
+        }
+        return root;
+    }
+}
