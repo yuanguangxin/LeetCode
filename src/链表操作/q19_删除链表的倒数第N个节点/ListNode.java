962f808cf9436a9b7c357c8e8b809808f49a491a
yuanguangxin <274841922@qq.com>
2020-03-26 18:56:59 +0800

    add q19

---
+++ src/链表操作/q19_删除链表的倒数第N个节点/ListNode.java
@@ -0,0 +1,12 @@
+package 链表操作.q19_删除链表的倒数第N个节点;
+
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
---
+++ src/链表操作/q19_删除链表的倒数第N个节点/Solution.java
@@ -0,0 +1,25 @@
+package 链表操作.q19_删除链表的倒数第N个节点;
+
+/**
+ * 利用两个指针 o(n)
+ */
+public class Solution {
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode first = dummy;
+        ListNode second = dummy;
+
+        for (int i = 1; i <= n + 1; i++) {
+            first = first.next;
+        }
+
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        return dummy.next;
+    }
+}
